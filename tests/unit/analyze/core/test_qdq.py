34d7d9b5ccb521c8a8d31dba8361b97695b5758e
hualxie <hualxie@microsoft.com>
2026-04-02 13:40:28 +0800

    add test case

--- tests/unit/analyze/core/test_qdq.py
+++ tests/unit/analyze/core/test_qdq.py
@@ -1104,6 +1104,7 @@ class TestIterQDQCombinations:
                 1440,
             ),  # (3+3+6*4) * 4 QDQ * 4 attr combos
             # * 3 (axes none, const, not const)
+            ("Relu", unary_input_shapes * 4 * 2),
             ("Reshape", 36 * 4 * 2 * 2),  # allowzero 2 * is_constant 2
             (
                 "Resize",
