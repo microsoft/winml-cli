a32f3749ed679372601e23728539424bc53b2175
fangyangci <fangyangci@microsoft.com>
2026-07-22 21:22:25 +0800

    squash: consolidate diff against origin/main
    # Conflicts:
    #	src/winml/modelkit/pattern/op_input_gen/__init__.py

--- src/winml/modelkit/pattern/op_input_gen/__init__.py
+++ src/winml/modelkit/pattern/op_input_gen/__init__.py
@@ -2,11 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
-try:
-    from .attention_input_generator import AttentionInputGenerator
-except ModuleNotFoundError:
-    # Keep optional generator import from blocking unrelated pattern tooling.
-    AttentionInputGenerator = None
+from .attention_input_generator import AttentionInputGenerator
 from .binary_input_generator import *
 from .binary_like_input_generator import *
 from .constant_of_shape_input_generator import ConstantOfShapeInputGenerator
